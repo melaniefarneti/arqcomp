5c8d820045466dd8b252ded92cad887006c8dfe1
melaniefarneti <2113289@ucc.edu.ar>
2023-06-03 21:32:52 -0300

    .

--- proyecto.cpp
+++ proyecto.cpp
@@ -29,9 +29,19 @@ MOV PC,L       // return from main
 #define MAX_INTENTOS 3
 #define CLAVE "12345"
 
+void disp_binary (int i){
+    int t;
+    for(t=128; t>0; t=t/2)
+        if(i&t) printf("* ");
+        else printf("_ ");
+    printf("\n"); // Regresar al inicio de la línea
+
+}
+
 int main() {
     char claveIngresada[6]; // 5 dígitos + 1 para el carácter nulo '\0'
     int intentos = 0;
+    unsigned char choque[8] = {0x81, 0x42, 0x24, 0x18, 0x18, 0x24, 0x42, 0x81};
 
     printf("Ingrese su contrasenia de 5 digitos: ");
 
@@ -60,21 +70,29 @@ int main() {
             scanf("%d", &opcion);
 
             switch (opcion) {
-                case 1:
+                case 1: {
                     printf("Auto Fantastico\n");
                     break;
-                case 2:
+                }
+                case 2: {
                     printf("Choque\n");
+                    for (char i = 0; i <= 7; i++) {
+                        disp_binary(choque[i]);
+                    }
                     break;
-                case 3:
+                }
+                case 3: {
                     printf("Secuencia 1\n");
                     break;
-                case 4:
+                }
+                case 4: {
                     printf("Secuencia 2\n");
                     break;
-                default:
+                }
+                default: {
                     printf("Opcion invalida\n");
                     break;
+                }
             }
 
 
